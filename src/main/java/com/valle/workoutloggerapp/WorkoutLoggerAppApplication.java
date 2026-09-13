bda0ee96a67222096966e780829f5deb9493e611
andrewvalles <andrewvallescsw@gmail.com>
2026-09-12 17:43:18 -0700

    --new project-- --no changes, ignore--

---
+++ src/main/java/com/valle/workoutloggerapp/WorkoutLoggerAppApplication.java
@@ -0,0 +1,13 @@
+package com.valle.workoutloggerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutLoggerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutLoggerAppApplication.class, args);
+    }
+
+}
